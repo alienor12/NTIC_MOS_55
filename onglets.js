4a13d7bdfd7b6d03bf2de76a31e5d3817e45b647
alienor12 <alienor95@hotmail.fr>
2019-06-02 20:09:17 +0200

    Add files via upload

---
+++ onglets.js
@@ -0,0 +1,37 @@
+$(function() {
+	$('#onglets').css('display', 'block');
+	$('#onglets').click(function(event) {
+		var actuel = event.target;
+		if (!/li/i.test(actuel.nodeName) || actuel.className.indexOf('actif') > -1) {
+			return;
+		}
+		$(actuel).addClass('actif').siblings().removeClass('actif');
+		setDisplay();
+	});
+	function setDisplay() {
+		var modeAffichage;
+		$('#onglets li').each(function(rang) {
+			modeAffichage = $(this).hasClass('actif') ? '' : 'none';
+			$('.item').eq(rang).css('display', modeAffichage);
+		});
+	}
+	setDisplay();
+});
+$(function(){
+	$("#presentation").load("presentation.html"); 
+});
+$(function(){
+	$("#arbitrage").load("arbitrage.html"); 
+});
+$(function(){
+	$("#diffusion").load("retransmission.html"); 
+});
+$(function(){
+	$("#performance").load("performance.html"); 
+});
+$(function(){
+	$("#blessure").load("blessure.html"); 
+});
+$(function(){
+	$("#tactique").load("tactique.html"); 
+});
